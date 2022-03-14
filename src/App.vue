888a38ccdbcb40f4e0c8009d82dbfdea9d798a49
MukhametovUR <ural-lan4x4@yandex.ru>
2022-03-14 14:59:32 +0300

    Создана разметка

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,87 @@
 <template>
-    <h1> {{ message }} </h1>
-    <button @click="increment"> {{ count }}</button>
+    <div class="app">
+      <h1> Планировщик задач </h1>
+      <div>
+        <h3>Добавить задачу</h3>
+        <form @click="createTask">
+          <input 
+          class="input"
+          type="text"
+          placeholder="Ответственный">
+          <input 
+          class="input"
+          type="text"
+          placeholder="Тема задачи">
+          <button class="btn">Создать задачу</button>
+        </form>
+        <div 
+        v-for="item in tasks" :key="item.id"
+        class="taskList">
+          <div> Задача: {{ item.id }} </div>
+          <div> Ответственный: {{ item.userId }} </div>
+          <div> Описание: {{ item.title }} </div>
+          <div> Статус: {{ item.completed }} </div>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        message:"Hello",
-        count: 0
+        tasks: [
+          {userId:1, id:1, title:'Проектирование проекта',completed:false},
+          {userId:2, id:2, title:'Создать проект',completed:false},
+          {userId:3, id:3, title:'Создать комопненты',completed:false},
+          {userId:4, id:4, title:'Сделать декомпозицию',completed:false},
+        ],
+        title:'',
+        body:''
       }
     },
     methods: {
-      increment(){
-        this.count++
-      }
-    },
-    mounted(){
-      this.increment();
+
     }
   }
 </script>
 
 
 <style>
-@import './assets/base.css';
-
+/* @import '@/assets/base.css'; */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 12px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.taskList {
+  margin: 10px;
+  display: flex;
+  border: 1px solid teal;
+  flex-direction: column;
+  padding: 15px;
+  border-radius: 5px;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  border-radius: 5px;
+}
 </style>
