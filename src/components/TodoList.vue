e363ffcc7005062e96c3d25bda9016cbfe08a825
MukhametovUR <ural-lan4x4@yandex.ru>
2022-03-15 19:05:55 +0300

    Условный рендеринг если список задач пуст

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,5 @@
 <template>
-     <div>
+     <div v-if="tasks.length > 0">
          <h3>Список задач</h3>
          <todo-item 
          v-for="task in tasks" :key="task.id"
@@ -7,6 +7,7 @@
          @remove="$emit('remove', task)"
          />
      </div>
+     <h2 v-else style="color:red">Список задач пуст</h2>
 </template>
 <script>
 import TodoItem from '@/components/TodoItem.vue';
