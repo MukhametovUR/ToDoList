fc3b5af9b4d7b1676acd6c3a9762897b99f2d2a0
MukhametovUR <ural-lan4x4@yandex.ru>
2022-03-18 18:44:23 +0300

    Анимированная сортировка, удаление, добавление

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,14 +1,13 @@
 <template>
-     <div v-if="tasks.length > 0"
-            class="reverse"
-         >
-         <todo-item 
-            v-for="task in tasks" :key="task.id"
-            :task="task"
-            @remove="$emit('remove', task)"
-         />
+     <div v-if="tasks.length > 0" class="reverse">  
     <h2 class="title__tasks">Список задач</h2>
-
+          <transition-group name="list">
+                <todo-item 
+                    v-for="task in tasks" :key="task.id"
+                    :task="task"
+                    @remove="$emit('remove', task)"
+                />        
+        </transition-group>  
      </div>
      <h2 v-else style="color:red">Список задач пуст</h2>
 </template>
@@ -25,12 +24,27 @@ export default {
     }
 }
 </script>
-<style>
+<style >
 .reverse{
     display: flex;
     flex-direction: column-reverse;
 }
 .title__tasks {
     margin-top: 20px;
+}
+
+.list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 0.4s;
+}
+.list-enter, .list-leave-to /* .list-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.list-move {
+  transition: transform 0.3s;
 }
 </style>
